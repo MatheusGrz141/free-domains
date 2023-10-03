c9ca9ab1b95fac41138405ff7842c5a51d3e3adf
Grzibovski.1410 <matheusgvieira141@gmail.com>
2023-10-03 19:47:02 -0400

    create bestrong.is-an.app.js and bestrong.api.is-an.app.js

---
+++ domains/bestrong.api.is-an.app.js
@@ -0,0 +1,11 @@
+addSubDomain({
+  description: 'A api for an personal project',
+  domain: 'is-an.app',
+  subdomain: 'bestrong.api',
+  owner: {
+    email: 'ladsprojeto@gmail.com',
+  },
+  record: {
+    A: ['18.230.150.163']
+  }
+});
---
+++ domains/bestrong.is-an.app.js
@@ -0,0 +1,11 @@
+addSubDomain({
+  description: 'A website for an personal project',
+  domain: 'is-an.app',
+  subdomain: 'bestrong',
+  owner: {
+    email: 'ladsprojeto@gmail.com',
+  },
+  record: {
+    A: ['18.230.150.163']
+  }
+});
